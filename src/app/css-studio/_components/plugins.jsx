2e98fcc5a9cc19a872cf27d95dae25c6188b107c
RFLI <141246668+Rafli161102@users.noreply.github.com>
2026-04-02 02:31:11 +0700

    Update plugins.jsx

--- src/app/css-studio/_components/plugins.jsx
+++ src/app/css-studio/_components/plugins.jsx
@@ -1,8 +1,4 @@
 "use client";
-
-// Ini adalah "Barrel File" yang bertugas mengumpulkan semua fitur 
-// dari file pecahan agar page.jsx tidak kebingungan mencarinya.
-
 export * from './pluginCore';
 export * from './pluginPro';
 export * from './pluginMax';
